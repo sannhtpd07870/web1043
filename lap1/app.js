a007bb0723f4b3a6189086b66bc20476efe6c33b
thai san <sannhtpd07870@fpt.edu.vn>
2023-03-09 23:07:40 +0700

    message

---
+++ lap1/app.js
@@ -0,0 +1,14 @@
+// khai báo biến a, tạo hộp thoại lấy dữ liệu từ bàn phím
+let a= prompt("moi ban nhap so nguyen a:");
+// ép kiểu số nguyên
+let a1= parseInt(a);
+// khai báo biến b, tạo hộp thoại lấy dữ liệu từ bàn phím
+let b= prompt("moi ban nhap so nguyen b:");
+// ép kiểu số nguyên
+let b1= parseInt(b);
+// tạo một biến c được chứa kết quả
+let c= a1 + b1;
+// hiện thị kết quả ra màn hình bằng document.write
+document.write(c);
+// hiện thị kết quả ra màn hình bằng console.log
+console.log(c);
